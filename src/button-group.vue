5ba9392528e149a78e6ec543e8fcfbed113002a1
BraveWangDev <BraveWangDev@126.com>
2020-08-07 06:36:47 +0800

    创建ButtonGroup

---
+++ src/button-group.vue
@@ -0,0 +1,33 @@
+<template>
+    <!--不能直接使用slot作为根节点，需要加 div-->
+    <div class="b-button-group">
+        <slot></slot>
+    </div>
+</template>
+<script></script>
+<style lang="scss">
+    .b-button-group{
+        display: inline-flex;
+        vertical-align: middle;
+        > .b-button{                /* b-button-group内部的b-button */
+            /* 解决按钮组内部button边缘重合问题：*/
+            /*&:not(:first-child){    !* 方案一：如果不是第一个元素再加上 *!*/
+                /*border-left: none;  !* 存在BUG：没有了左侧 border *!*/
+            /*}*/
+            border-radius: 0;       /* 重置border-radius */
+            margin-left: -1px;      /* 方案二：让右边的border盖住左边的border */
+            &:first-child{          /* b-button-group内部的第一个b-button */
+                border-top-left-radius: var(--border-radius);
+                border-bottom-left-radius: var(--border-radius);
+            }
+            &:last-child{           /* b-button-group内部的最后一个b-button */
+                border-top-right-radius: var(--border-radius);
+                border-bottom-right-radius: var(--border-radius);
+            }
+            &:hover{                 /* 控制 hover 时选中的控件边框处于上层 */
+                position: relative;
+                z-index: 1;
+            }
+        }
+    }
+</style>
