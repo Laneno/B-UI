3929b7bf6d542a3ee43223e44b68c072d035cee5
BraveWangDev <BraveWangDev@126.com>
2020-08-07 20:11:48 +0800

    使用JS创建Button组件、Button组件的单元测试：icon参数

--- src/app.js
+++ src/app.js
@@ -15,3 +15,24 @@ new Vue({
         loading3: false     // 右loading按钮
     }
 })
+
+// 单元测试
+import chai from 'chai'
+const expect = chai.expect;
+{
+    // console.log(Button)                  // 打印 Button 组件：是一个对象
+    // 动态生成一个按钮
+    const Constructor = Vue.extend(Button); // 将Button组件变成一个构造函数(Button是一个VueComponent函数)
+    // 通过 VueComponent 函数构造一个button
+    const button = new Constructor({        // 通过new button的构造函数,创建一个button实例
+        propsData: {
+            icon: 'settings'
+        }
+    });
+    button.$mount('#test')                  // 将button实例挂载到test上
+    let useElement = button.$el.querySelector('use')    // 在button元素中找use元素
+    console.log(useElement)
+    let href = useElement.getAttribute('xlink:href')    // 拿到useElement中xlink:href元
+    expect(href).to.eq('settings')                      // 断言: 我主观认为useElement中xlink:href元素值为settings
+}
+
