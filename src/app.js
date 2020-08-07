8b430b545be7d2b1ecdd868a1f2282e208fff716
BraveWangDev <BraveWangDev@126.com>
2020-08-07 20:21:33 +0800

    Button组件的单元测试：loading参数

--- src/app.js
+++ src/app.js
@@ -19,6 +19,8 @@ new Vue({
 // 单元测试
 import chai from 'chai'
 const expect = chai.expect;
+
+// Button组件的单元测试：icon参数
 {
     // console.log(Button)                  // 打印 Button 组件：是一个对象
     // 动态生成一个按钮
@@ -33,6 +35,22 @@ const expect = chai.expect;
     let useElement = button.$el.querySelector('use')    // 在button元素中找use元素
     console.log(useElement)
     let href = useElement.getAttribute('xlink:href')    // 拿到useElement中xlink:href元
-    expect(href).to.eq('settings')                      // 断言: 我主观认为useElement中xlink:href元素值为settings
+    expect(href).to.eq('#i-settings')                      // 断言: 我主观认为useElement中xlink:href元素值为settings
 }
 
+
+// Button组件的单元测试：loading参数
+{
+    const Constructor = Vue.extend(Button);
+    const button = new Constructor({
+        propsData: {
+            icon: 'settings',
+            loading: true               // 添加loading参数
+        }
+    });
+    button.$mount()                     // $mount不写就是放到内存中
+    let useElement = button.$el.querySelector('use')
+    console.log(useElement)
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#i-loading')    // loading 为 true 时，settings 是看不到的，所以断言值为#i-loading
+}
